ddc5f40247ea06d75a24027d97a749453380cd3b
Alexandr Korsak <alex.korsak@gmail.com>
2014-07-16 17:38:16 +0300

    Add spec for batches API

--- spec/mongoid/report/threads_spec.rb
+++ spec/mongoid/report/threads_spec.rb
@@ -37,4 +37,48 @@ describe Mongoid::Report do
 
     time2.real.should > time1.real
   end
+
+  it 'should work faster using batches in threads on aggregate' do
+    Report1 = Class.new do
+      include Mongoid::Report
+
+      report 'example' do
+        attach_to Model do
+          group_by :day
+          column :field1
+        end
+      end
+    end
+
+    Report2 = Class.new do
+      include Mongoid::Report
+
+      report 'example' do
+        attach_to Model do
+          group_by :day
+          batches size: 5, by: :day
+          column :field1
+        end
+      end
+    end
+
+    5.times do |i|
+      10000.times { klass.create!(day: i.days.ago, field1: 1) }
+    end
+
+    time1 = Benchmark.measure do
+      rows = scoped
+        .in_batches(day: (0.days.ago.to_date..5.days.from_now.to_date))
+        .all
+    end
+
+    time2 = Benchmark.measure do
+      rows = scoped.all
+    end
+
+    puts time2
+    puts time1
+
+    time2.real.should > time1.real
+  end
 end
