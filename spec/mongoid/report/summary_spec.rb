574beb6a38ee51a3344e9752092544e721237527
Kirill Shaplyko <k.shaplyko@gmail.com>
2014-07-18 13:35:24 +0300

    Fix summary calculating for dynamic columns

--- spec/mongoid/report/summary_spec.rb
+++ spec/mongoid/report/summary_spec.rb
@@ -12,12 +12,12 @@ describe Mongoid::Report do
       klass.create!(day: today     , field1: 1)
       klass.create!(day: yesterday , field1: 1)
 
-      Report = Class.new do
+      Report1 = Class.new do
         include Mongoid::Report
         group_by :day, for: Model
         column :field1, for: Model
       end
-      example = Report.new
+      example = Report1.new
 
       report = example.aggregate_for(klass)
       report = report.all
@@ -28,42 +28,46 @@ describe Mongoid::Report do
     end
 
     it 'should support dynamic columns as well' do
-      Report = Class.new do
+      Report2 = Class.new do
         include Mongoid::Report
 
         COLUMNS = {
           :'new-field1' => ->(context, row, options) { row['field1'] * 10 },
-          :'new-field2' => ->(context, row, options) { row['field1'] * 20 },
+          :'new-field2' => ->(context, row, options) { row['field2'] * 20 },
         }
 
         report 'example' do
           attach_to Model do
             columns COLUMNS
-            column :field1, 'new-field1'
+            column :field1, :field2, 'new-field1', 'new-field2'
           end
         end
       end
 
-      klass.create!(field1: 1)
-      klass.create!(field1: 1)
-      klass.create!(field1: 1)
+      klass.create!(field1: 1, field2: 3)
+      klass.create!(field1: 2, field2: 2)
+      klass.create!(field1: 3, field2: 1)
 
-      report = Report.new
+      report = Report2.new
       report = report.aggregate_for('example-models')
       report = report.all
       rows = report.rows
 
-      expect(rows[0].keys.size).to eq(2)
-      expect(rows[0]['field1']).to eq(3)
-      expect(rows[0]['new-field1']).to eq(30)
+      expect(rows[0].keys.size).to eq(4)
+      expect(rows[0]['field1']).to eq(6)
+      expect(rows[0]['field2']).to eq(6)
+      expect(rows[0]['new-field1']).to eq(60)
+      expect(rows[0]['new-field2']).to eq(120)
 
-      expect(report.summary.keys.size).to eq(2)
-      expect(report.summary['field1']).to eq(3)
-      expect(report.summary['new-field1']).to eq(30)
+      expect(report.summary.keys.size).to eq(4)
+      expect(report.summary['field1']).to eq(6)
+      expect(report.summary['field2']).to eq(6)
+      expect(report.summary['new-field1']).to eq(60)
+      expect(report.summary['new-field2']).to eq(120)
     end
 
     it 'should not summaries day field' do
-      Report = Class.new do
+      Report3 = Class.new do
         include Mongoid::Report
 
         report 'example' do
@@ -78,7 +82,7 @@ describe Mongoid::Report do
       klass.create!(day: DateTime.now, field1: 1)
       klass.create!(day: DateTime.now, field1: 1)
 
-      report = Report.new
+      report = Report3.new
       report = report.aggregate_for('example-models')
       report = report.all
       rows = report.rows
